a84996bcd587cfb25b1658db2e7d24d470fa587e
madykadzheffri@gmail.com <>
2025-01-06 18:30:58 +0200

    no message

---
+++ tets10/tets10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tets10
+//
+//  Created by Djeffri on 06.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+let a = 10
+    let b = 10
+}
+
